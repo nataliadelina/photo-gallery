1bbc6a6a8d30b642a361adff3d138aa5d764a996
Natalia <natalia_alexandra@web.de>
2021-12-01 20:26:12 +0100

    add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <p>&copy; <span class="bold"> O.N</span>atalia</p>
+  </footer>
+</template>
+
+<style>
+footer {
+  padding: 1rem;
+  text-align: center;
+  color: rgb(243, 240, 245);
+  background-color: rgb(45, 42, 49);
+}
+.bold {
+  font-weight: bold;
+}
+</style>
